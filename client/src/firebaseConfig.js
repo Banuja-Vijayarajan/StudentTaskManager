2a1dac0d7a3ecd948b2f5da332ef05dad8f7d425
Banuja-Vijayarajan <vbanuja22@gmail.com>
2026-07-07 16:14:38 +0530

    Initial project setup

---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAeZpXh5jizENmSEO9td5kQw7rNJ1qW9Ho",
+    authDomain: "project-1-8120c.firebaseapp.com",
+    projectId: "project-1-8120c",
+    storageBucket: "project-1-8120c.firebasestorage.app",
+    messagingSenderId: "430842930733",
+    appId: "1:430842930733:web:07f5f488cf9009b1f07aa4"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
