e464a8c739823217f76fc90ce0dbeb58ed905930
Banuja-Vijayarajan <vbanuja22@gmail.com>
2026-07-08 20:32:38 +0530

    Implement Firebase user registration

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,44 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 import "../styles/Register.css";
 
 import Button from "../components/Button";
 import Input from "../components/Input";
 import Card from "../components/Card";
 
+import { registerUser } from "../services/auth";
+
 function Register() {
+    const navigate = useNavigate();
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function handleRegister() {
+
+    if (!name || !email || !password) {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    try {
+
+        await registerUser(email, password);
+
+        alert("Registration Successful!");
+
+        navigate("/");
+
+    } catch (error) {
+
+        alert(error.message);
+
+    }
+
+}
+
   return (
     <div className="register-container">
 
@@ -18,21 +51,30 @@ function Register() {
         </p>
 
         <Input
-          type="text"
-          placeholder="Full Name"
+            type="text"
+            placeholder="Full Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
         />
 
         <Input
-          type="email"
-          placeholder="Email"
-        />
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+        /> 
 
         <Input
-          type="password"
-          placeholder="Password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
         />
 
-        <Button text="Create Account" />
+        <Button
+            text="Create Account"
+            onClick={handleRegister}
+        />
 
         <p className="login-text">
           Already have an account?
